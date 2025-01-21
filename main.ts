ef4400a6417146dfed1df97127cbd9fc4a46d7b3
lucas-nguyen0 <lucas.nguyen@mths.ca>
2025-01-21 10:19:27 -0500

    code works

--- main.ts
+++ main.ts
@@ -1,8 +1,55 @@
 /* Copyright (c) 2020 MTHS All rights reserved
  *
- * Created by: Mr. Coxall
- * Created on: Sep 2020
- * This program ...
+ * Created by: Lucas Nguyen
+ * Created on: Jan 2025
+ * This program receives a radio signal and spins a servo motor.
 */
 
-basic.showString('Hello, World!')
+// variables
+const servoNumber1 = robotbit.Servos.S1
+
+// setup
+radio.setGroup(142)
+basic.showLeds(`
+. # # # .
+. # . # .
+# # # # #
+# . . . #
+# # # # #
+`)
+robotbit.Servo(servoNumber1, 0)
+
+// correct combo inputted
+radio.onReceivedString(function(receivedString) {
+    basic.showLeds(`
+    . # # # .
+    . . . # .
+    # # # # #
+    # . . . #
+    # # # # #
+    `)
+    robotbit.Servo(servoNumber1, 90)
+})
+
+// lock reset
+input.onButtonPressed(Button.A, function () {
+    basic.showLeds(`
+    . # # # .
+    . . . # .
+    # # # # #
+    # . . . #
+    # # # # #
+    `)
+    robotbit.Servo(servoNumber1, 90)
+})
+
+input.onButtonPressed(Button.B, function () {
+    basic.showLeds(`
+    . # # # .
+    . # . # .
+    # # # # #
+    # . . . #
+    # # # # #
+    `)
+    robotbit.Servo(servoNumber1, 0)
+})
